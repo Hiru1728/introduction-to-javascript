64a1b5d9db6efbf6337f5752793c898d9fec3d0e
unknown <Hiru1728>
2022-07-31 18:11:20 +0600

    javascript introdction

---
+++ remainder.js
@@ -0,0 +1,8 @@
+var mangoes = 17;
+var person = 5;
+
+// / this means vagfol
+// console.log(mangoes / person);
+// % this means vagses module operator
+var remainder = mangoes % person;
+console.log(remainder);
---
+++ variable-name.js
@@ -0,0 +1,32 @@
+//good variable
+var price = 29;
+
+VAR name = "John";
+Var name = "John";
+//1.variable name can not any keywords
+var false = 96;
+var return = true;
+
+//variable name has to be in one work. No space
+var my home address = "New California"
+
+//3.variable name can not have quoatation
+var "name" = "Tom Hanks";
+
+//4. variable name can not starts with a number but can end with a number
+var 99Club = 1964;
+var club25 = 2025;
+
+// 5.How to use long names
+// can't use dash
+var user-name="raj Bappa";
+
+var user_name = "raj bappa";
+var user_current_home_address = "andor killa";
+var use_home_address = "andor killa bandor ban";
+var use_home_address = "andor killa bandor ban";//snake case
+var useHomeAddress = "andor killa bandor ban"; //camel case(we will use this one)
+var UseHomeAddress = "andor killa bandor ban";//pascal case
+// variable name is case sensitive
+var person = 25;
+var Person = 35;
